7feac9444fd9285793d938301bf00ca676400761
Alan Colon <alco@uei.com>
2017-07-19 11:32:28 -0700

    Fixed eof bug with readline

--- observable.js
+++ observable.js
@@ -1,9 +1,19 @@
 const rx = require('rxjs')
 const rxx = require('./rxx')
-const fromReadLine = (rl) => rx.Observable.create(obs => rl
-  .on('line', obs.next.bind(obs))
-  .on('close', obs.complete.bind(obs))
-)
+const readline = require('readline')
+
+const fromReadLine = (rl) => rx.Observable.create(obs => {
+  if (!rl) {
+    rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+  }
+  rl
+    .on('line', obs.next.bind(obs))
+    .on('close', obs.complete.bind(obs))
+  return () => rl.close()
+})
 const fromStream = (stream) => rx.Observable.create(obs => {
   stream.setEncoding('utf8')
   stream.on('readable', () => obs.next(process.stdin.read()))
